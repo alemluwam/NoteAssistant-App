a4bbcdce65c1d15d492c7368666a383f37134138
sikenman <sikenman@yahoo.com>
2016-04-20 11:35:07 -0500

    NoteDao

--- src/main/java/edu/mum/cs544/dao/NoteDao.java
+++ src/main/java/edu/mum/cs544/dao/NoteDao.java
@@ -1,9 +1,9 @@
 package edu.mum.cs544.dao;
 
-import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import edu.mum.cs544.domain.Note;
@@ -11,20 +11,44 @@ import edu.mum.cs544.domain.Note;
 @Repository
 public interface NoteDao extends JpaRepository<Note, Long> {
 
-	List<Note> findByTitle(String title);
+	// For Searching
+	/* giving some problems 
+	@Query("SELECT n FROM Note n WHERE n.section.sectionId = ?2 and n.title LIKE CONCAT(?1, '%') ORDER BY n.title")
+	List<Note> findByTitle(String title, Long sectionid);
 
-	List<Note> findByContent(String content);
+	@Query("SELECT n FROM Note n WHERE n.section.sectionId = ?2 and n.content LIKE CONCAT('%', ?1, '%') ORDER BY n.createdDate")
+	List<Note> findByContent(String content, Long sectionid);
 
-	List<Note> findByLink(String link);
+	@Query("SELECT n FROM Note n WHERE n.section.sectionId = ?2 and n.link LIKE CONCAT('%', ?1, '%') ORDER BY n.createdDate")
+	List<Note> findByLink(String link, Long sectionid);
 
-	List<Note> findAllByOrderByCreatedDateAsc(Date createdDate);
+	@Query("SELECT n FROM Note n WHERE n.section.sectionId = ?2 and n.remark LIKE CONCAT('%', ?1, '%') ORDER BY n.createdDate")
+	List<Note> findByRemark(String remark, Long sectionid);
+	*/
+	
+	//select a from Author as a join a.Book as ab where ab.AuthorId like '%"hello"%';
+	
+	@Query("SELECT n FROM Note n WHERE n.section.sectionId = ?2 and n.title LIKE ?1% ORDER BY n.title")
+	List<Note> findByTitle(String title, Long sectionId);
 
-	List<Note> findAllByOrderByCreatedDateDesc(Date createdDate);
+	@Query("SELECT n FROM Note n WHERE n.section.sectionId = ?2 and n.content LIKE %?1% ORDER BY n.createdDate")
+	List<Note> findByContent(String content, Long sectionId);
 
-	List<Note> findAllByOrderByModifiedDateAsc(Date modifiedDate);
+	@Query("SELECT n FROM Note n WHERE n.section.sectionId = ?2 and n.link LIKE CONCAT('%', ?1, '%') ORDER BY n.createdDate")
+	List<Note> findByLink(String link, Long sectionId);
 
-	List<Note> findAllByOrderByModifiedDateDesc(Date modifiedDate);
+	@Query("SELECT n FROM Note n WHERE n.section.sectionId = ?2 and n.remark LIKE CONCAT('%', ?1, '%') ORDER BY n.createdDate")
+	List<Note> findByRemark(String remark, Long sectionId);
 
-	List<Note> findAllByOrderByTitleAsc(String title);
+	
+	// For Sorting Part
+	List<Note> findAllByOrderByCreatedDateAsc();
+	List<Note> findAllByOrderByCreatedDateDesc();
+
+	List<Note> findAllByOrderByModifiedDateAsc();
+	List<Note> findAllByOrderByModifiedDateDesc();
+
+	List<Note> findAllByOrderByTitleAsc();
+	List<Note> findAllByOrderByTitleDesc();
 
 }
